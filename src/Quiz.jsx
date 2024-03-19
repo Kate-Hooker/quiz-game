2c6cfd1a9024dc0978da346ebdd08bacfc5f5240
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-19 21:45:07 +1300

    questions set as const containing data from js array, questions.length added to count number of qs in quiz

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -3,10 +3,13 @@ import { useState } from 'react'
 const Quiz = ({ questions }) => {
   const [currentQuestion, setCurrentQuestion] = useState()
 
+  const { question, choices, correctAnswer } = questions[currentQuestion]
+
   return (
     <div className="quiz-container">
       <>
-        <span className="active-question-no"></span>
+        <span className="active-question-no">{currentQuestion + 1}</span>
+        <span className="totalQuestions">{question.length}</span>
       </>
     </div>
   )
