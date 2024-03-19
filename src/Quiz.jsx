fbc45eca8683e3082e52b184b1cd8240831f9a3d
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-19 22:37:32 +1300

    question added to quiz container renders in quiz container w/ question counter

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -10,6 +10,7 @@ const Quiz = ({ questions }) => {
       <>
         <span className="active-question-no">{currentQuestion + 1}</span>
         <span className="total-questions">/{questions.length}</span>
+        <h2>{question}</h2>
       </>
     </div>
   )
