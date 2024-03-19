d513b75ecb070219e663b442f6bae3ccc8bdc300
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-20 12:42:37 +1300

    map used to list multi choices under Q, onClick added to reveal answer

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -5,12 +5,19 @@ const Quiz = ({ questions }) => {
 
   const { question, choices, correctAnswer } = questions[currentQuestion]
 
+  const onAnswerClick = (answer, index) => {}
+
   return (
     <div className="quiz-container">
       <>
         <span className="active-question-no">{currentQuestion + 1}</span>
         <span className="total-questions">/{questions.length}</span>
         <h2>{question}</h2>
+        <ul>
+          {choices.map((answer, index) => (
+            <li onClick={() => onAnswerClick(answer, index)} key={answer}></li>
+          ))}
+        </ul>
       </>
     </div>
   )
