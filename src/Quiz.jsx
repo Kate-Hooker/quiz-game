2851bc1046a6a88a47fadc2244b1866bcd621832
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-19 21:38:10 +1300

    useState added to quiz questions (setQuestion, current question)

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,9 +1,15 @@
-const Quiz = ({questions}) => {
+import { useState } from 'react'
+
+const Quiz = ({ questions }) => {
+  const [currentQuestion, setCurrentQuestion] = useState()
+
   return (
     <div className="quiz-container">
-      
+      <>
+        <span className="active-question-no"></span>
+      </>
     </div>
   )
 }
 
-export default Quiz;
+export default Quiz
