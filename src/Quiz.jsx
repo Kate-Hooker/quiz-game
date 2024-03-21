a6dbd5c7f893b37d585a61cf7767876cae33d131
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-21 17:44:01 +1300

    added totaled score, rights and wrongs to results page

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -3,7 +3,6 @@ import { resultInitialState } from './costants'
 
 const Quiz = ({ questions }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0)
-
   const [answerIdx, setAnswerIdx] = useState(null)
   const [answer, setAnswer] = useState(null)
   const [result, setResult] = useState(resultInitialState)
@@ -18,12 +17,8 @@ const Quiz = ({ questions }) => {
             score: prev.score + 5,
             correctAnswers: prev.correctAnswers + 1,
           }
-        : {
-            ...prev,
-            wrongAnswers: prev.wrongAnswers + 1,
-          }
+        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
     )
-
     if (currentQuestion !== questions.length - 1) {
       setCurrentQuestion((prev) => prev + 1)
     } else {
@@ -45,27 +40,45 @@ const Quiz = ({ questions }) => {
 
   return (
     <div className="quiz-container">
-      <>
-        <span className="active-question-no">{currentQuestion + 1}</span>
-        <span className="total-questions">/{questions.length}</span>
-        <h2>{question}</h2>
-        <ul>
-          {choices.map((answer, index) => (
-            <li
-              onClick={() => onAnswerClick(answer, index)}
-              key={answer}
-              className={answerIdx === index ? 'selected-answer' : null}
-            >
-              {answer}
-            </li>
-          ))}
-        </ul>
-        <div className="footer">
-          <button onClick={onClickNext} disabled={answerIdx === null}>
-            {currentQuestion === questions.length - 1 ? 'Finish' : 'Next'}
-          </button>
+      {!showResult ? (
+        <div>
+          <span className="active-question-no">{currentQuestion + 1}</span>
+          <span className="total-questions">/{questions.length}</span>
+          <h2>{question}</h2>
+          <ul>
+            {choices.map((answer, index) => (
+              <li
+                onClick={() => onAnswerClick(answer, index)}
+                key={answer}
+                className={answerIdx === index ? 'selected-answer' : null}
+              >
+                {answer}
+              </li>
+            ))}
+          </ul>
+          <div className="footer">
+            <button onClick={onClickNext} disabled={answerIdx === null}>
+              {currentQuestion === questions.length - 1 ? 'Finish' : 'Next'}
+            </button>
+          </div>
         </div>
-      </>
+      ) : (
+        <div className="result">
+          <h3>Result</h3>
+          <p>
+            Total Questions: <span>{questions.length}</span>
+          </p>
+          <p>
+            Total Score: <span>{result.score}</span>
+          </p>
+          <p>
+            Total Correct Answers: <span>{result.correctAnswers}</span>
+          </p>
+          <p>
+            Total Wrong Answers: <span>{result.wrongAnswers}</span>
+          </p>
+        </div>
+      )}
     </div>
   )
 }
